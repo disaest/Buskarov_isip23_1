eb72307d1db3e1a3c4188ac0df6174ef61d26f2c
disaest <buserov@icloud.com>
2025-12-17 10:39:43 +0900

    Первые 4 задания

---
+++ buskarov.py
@@ -0,0 +1,43 @@
+#1
+# x1 = int(input())
+# y1 = int(input())
+# x2 = int(input())
+# y2 = int(input())
+# if x1 > 8 or y1 > 8 or x2 > 8 or y2 > 8 or x1 < 1 or y1 < 1 or x2 < 1 or y2 < 1:
+#     print("Ошибка!")
+# else:
+#     dx = abs(x1 - x2)
+#     dy = abs(y1 - y2)
+#     if (dx == 1 and dy == 2) or (dx == 2 and dy == 1):
+#         print("Да")
+#     else:
+#         print("Нет")
+
+#2
+# k = int(input())
+# n = int(input())
+# sum = 0
+# while(k <= n):
+#     if k % 2 == 0:
+#         sum += k
+#         k += 1
+#     else:
+#         k += 1
+# print(sum)
+
+#3
+# a = 0
+# while True:
+#     b = int(input())
+#     if b == 0:
+#         break
+#     a += b
+# print(a)
+
+# #4
+# n = int(input())
+# a = 1
+# for i in range(1, n + 1):
+#     a *= i
+# print(a)
+    
