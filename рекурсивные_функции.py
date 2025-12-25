45757f77ae4a6b09cc30925729d5bb078e9383e8
disaest <buserov@icloud.com>
2025-12-25 20:31:58 +0900

    Add files via upload

---
+++ рекурсивные_функции.py
@@ -0,0 +1,18 @@
+#1
+# n = int(input())
+# def factorial(n):
+#     if n == 1:
+#         return 1
+#     else:
+#         return n * factorial(n - 1)
+# print(factorial(n))
+
+#2
+# def remove_vowels(string):
+#     itog = ""
+#     glasnie = "aeiouy"
+#     for i in string:
+#         if i not in glasnie:
+#             itog += i
+#     return itog
+# print(remove_vowels('yabloko'))
